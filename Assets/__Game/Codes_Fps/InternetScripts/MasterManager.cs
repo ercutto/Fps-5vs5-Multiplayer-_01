be57c1ba18a3c5551d4bf6219b653e775fc8ac69
ercutto <ercumentti81@gmail.com>
2022-07-08 12:28:58 +0300

    General Internet settings
    -game setting cs
    -Master Manager cs
    -Network auto instantiate
    -Scriptable objects singletons
    -team management values added to  Master manager

---
+++ Assets/__Game/Codes_Fps/InternetScripts/MasterManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEditor;
+
+[CreateAssetMenu(menuName ="Singletons/MasterManager")]
+
+public class MasterManager : ScriptableObjectSingleton<MasterManager>
+{
+    [SerializeField]
+    private GameSettings _gameSettings;
+    public static GameSettings gameSettings { get { return Instance._gameSettings; } }
+    [SerializeField]
+    private List<NetworkPrefab> _networkedPrefabs = new List<NetworkPrefab>();
+
+    public static Cards card = null;
+    public static GunsStat gunsStat = null;
+    public static int nextPlayersTeam;
+
+    public static GameObject NetworkInstantiate(GameObject obj, Vector3 position, Quaternion rotatiton)
+    {
+        foreach (NetworkPrefab networkPrefab in Instance._networkedPrefabs)
+        {
+            if (networkPrefab.prefab == obj)
+            {
+                if (networkPrefab._path != string.Empty)
+                {
+                    GameObject result = PhotonNetwork.Instantiate(networkPrefab._path, position, rotatiton);
+                    return result;
+                }
+                else
+                {
+                    Debug.LogError("Path is empty for gameObject name: " + networkPrefab.prefab);
+                    return null;
+                }
+
+            }
+        }
+        return null;
+    }
+
+    public static void Updateteam()
+    {
+        if (nextPlayersTeam == 1)
+        {
+            nextPlayersTeam = 2;
+        }
+        else
+        {
+            nextPlayersTeam = 1;
+        }
+    }
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void PopulateNetrworkPrefabs()
+    {
+#if UNITY_EDITOR
+        Instance._networkedPrefabs.Clear();
+        GameObject[] results = Resources.LoadAll<GameObject>("");
+        for (int i = 0; i < results.Length; i++)
+        {
+            if (results[i].GetComponent<PhotonView>()!=null)
+            {
+                string path = AssetDatabase.GetAssetPath(results[i]);
+                Instance._networkedPrefabs.Add(new NetworkPrefab(results[i], path));
+            }
+        }
+    }
+#endif
+}
---
+++ Assets/__Game/Codes_Fps/InternetScripts/NetworkPrefab.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+[System.Serializable]
+public class NetworkPrefab
+{
+    public GameObject prefab;
+    public string _path;
+
+    public NetworkPrefab(GameObject obj,string path)
+    {
+        prefab = obj;
+        _path = PrefabPathModifier(path);
+    }
+
+    private string PrefabPathModifier(string path)
+    {
+        int extensionLength = System.IO.Path.GetExtension(path).Length;
+        int additionalLength = 10;
+        int startIndex = path.ToLower().IndexOf("resources");
+        if (startIndex == -1)
+            return string.Empty;
+        else
+            return path.Substring(startIndex + additionalLength, path.Length - (additionalLength + startIndex + extensionLength));
+    }
+}
